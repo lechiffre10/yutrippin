4993f7e8ed0489f643465965131e22eb0052de2c
ray-curran <rpcurr@gmail.com>
2016-03-28 18:34:38 -0500

    Add query from index search page on click of submit button

--- javascript/app.js
+++ javascript/app.js
@@ -1,7 +1,10 @@
 (function (){
 	var app = angular.module('yutrippin', []);
 	app.controller('TrippinController', function($scope){
-
+    $scope.query = '';
+    $scope.search = function(query) {
+      $scope.query = query;
+    }
 	});
 
 
