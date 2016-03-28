2a46763f8467a1a4892950ff350e20765f2a568a
ray-curran <rpcurr@gmail.com>
2016-03-28 18:50:58 -0500

    Add provider for google maps in controller

--- javascript/app.js
+++ javascript/app.js
@@ -1,11 +1,20 @@
 (function (){
 	var app = angular.module('yutrippin', ['uiGmapgoogle-maps']);
-	app.controller('TrippinController', function($scope){
+  app.config(function(uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+        //    key: 'your api key',
+        v: '3.20', //defaults to latest 3.X anyhow
+        libraries: 'geometry'
+      });
+  })
+
+  app.controller('TrippinController', function($scope, uiGmapGoogleMapApi){
     $scope.query = '';
     $scope.search = function(query) {
       $scope.query = query;
     }
-	});
+  });
+
 
 
 
