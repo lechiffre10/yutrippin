4a2d8d74549e839ff5a57828509e14232fa60e58
[01;31m[K"name"[m[K: "Sabri Helal <[01;31m[K"email"[m[K: null>
2016-03-28 19:12:12 -0500

    Add geocoder

--- javascript/app.js
+++ javascript/app.js
@@ -2,7 +2,7 @@
 	var app = angular.module('yutrippin', ['uiGmapgoogle-maps']);
   app.config(function(uiGmapGoogleMapApiProvider) {
     uiGmapGoogleMapApiProvider.configure({
-        //    key: 'your api key',
+           key: 'AIzaSyAPgT3-JSaNBQ8cwyogdN1NeMRTmseoLDM',
         v: '3.20', //defaults to latest 3.X anyhow
         libraries: 'geometry'
       });
@@ -12,10 +12,13 @@
     $scope.query = '';
     $scope.search = function(query) {
       $scope.query = query;
+      var geocoder = new google.maps.Geocoder();
+      geocoder.geocode({'address': query}, function(results){
+        $scope.lat = results[0].geometry.location.lat();
+        $scope.lon = results[0].geometry.location.lng();
+        alert($scope.lat);
+        alert($scope.lon);
+      })
     }
-  });
-
-
-
-
+  })
 })();
