2348c0f937cf193fe20d7206b63e996342de1f73
ray-curran <rpcurr@gmail.com>
2016-04-09 11:42:25 -0500

    Add grid for pictures, styling, and f for buttons, show/hide content based on user flow

--- javascript/app.js
+++ javascript/app.js
@@ -2,7 +2,7 @@
 	var app = angular.module('yutrippin', ['uiGmapgoogle-maps', 'ngRoute', 'angularSoundManager']);
   app.config(function(uiGmapGoogleMapApiProvider) {
     uiGmapGoogleMapApiProvider.configure({
-     key: '',
+        key: '',
         v: '3.20', //defaults to latest 3.X anyhow
         libraries: 'geometry'
       });
@@ -12,6 +12,7 @@
     $scope.auth = window.location.hash.split('=')[1];
     $scope.query = '';
     $scope.instaPics = [];
+
     $scope.search = function(query) {
       $scope.query = query;
       $scope.mixTitle = query;
@@ -22,33 +23,45 @@
       })
       $scope.soundcloud();
       $scope.fetchPhotos();
+      $scope.query = '';
     }
+
     $scope.instagram = function() {
-      var url = 'https://api.instagram.com/oauth/authorize/?client_id=&redirect_uri=http://127.0.0.1:8080/&response_type=token';
+      var url = 'https://api.instagram.com/oauth/authorize/?client_id=9911045901584a84bf7ea98824e8ee85&redirect_uri=http://127.0.0.1:8080/&response_type=token';
       window.location.replace(url);
     }
+
     $scope.fetchPhotos = function(){
       $http.jsonp('https://api.instagram.com/v1/tags/chi/media/recent?access_token='+ $scope.auth + '&callback=JSON_CALLBACK').then(function(response){
         $scope.instaPics = response.data.data.filter(function(type){
           return type.type == "image";
         })
-        $scope.instaPics = $scope.instaPics.map(function(image){
+        .map(function(image){
          return image.images.standard_resolution.url;
        })
+
+        $scope.slicedPics = [];
+
+        while($scope.instaPics.length) {
+          $scope.slicedPics.push($scope.instaPics.splice(0,6));
+        }
+        console.log($scope.slicedPics);
       })
     }
+
     $scope.soundcloud = function (){
-      $http.get('http://api.soundcloud.com/tracks?client_id=&tag_list=' + $scope.query +'&q=' + $scope.query).then(function(response){
+      $http.get('http://api.soundcloud.com/tracks?client_id=d53072d1250a3396dfa2b6f9b56cc0b2&tag_list=' + $scope.query +'&q=' + $scope.query).then(function(response){
             $scope.sounds = response.data.map(function(song){
             return {
             id: song.id,
             title: song.title,
             artist: song.user.username,
-            url: song.stream_url + '?client_id='
+            url: song.stream_url + '?client_id=d53072d1250a3396dfa2b6f9b56cc0b2'
             }
         });
       })
     }
+
     $scope.playTrack = function(index){
       soundManager.pauseAll();
       window.sm2BarPlayers[0].actions.play(index);
