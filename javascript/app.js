38000812ded2e7d88640a2797ffe83d229f43513
[01;31m[K"name"[m[K: "Sabri Helal <[01;31m[K"email"[m[K: null>
2016-04-11 12:16:54 -0500

    Use flickr api instead of Instagram

--- javascript/app.js
+++ javascript/app.js
@@ -1,62 +1,43 @@
 (function (){
-	var app = angular.module('yutrippin', ['uiGmapgoogle-maps', 'ngRoute', 'angularSoundManager']);
-  app.config(function(uiGmapGoogleMapApiProvider) {
-    uiGmapGoogleMapApiProvider.configure({
-        key: '',
-        v: '3.20', //defaults to latest 3.X anyhow
-        libraries: 'geometry'
-      });
-  })
+	var app = angular.module('yutrippin', ['ngRoute', 'angularSoundManager']);
 
-  app.controller('TrippinController', function($scope, $http, uiGmapGoogleMapApi){
-    $scope.auth = window.location.hash.split('=')[1];
+
+  app.controller('TrippinController', function($scope, $http){
     $scope.query = '';
-    $scope.instaPics = [];
 
     $scope.search = function(query) {
+      $scope.currentPlaying = {};
       $scope.query = query;
       $scope.mixTitle = query;
-      var geocoder = new google.maps.Geocoder();
-      geocoder.geocode({'address': query}, function(results){
-        $scope.lat = results[0].geometry.location.lat();
-        $scope.lon = results[0].geometry.location.lng();
-      })
       $scope.soundcloud();
       $scope.fetchPhotos();
       $scope.query = '';
     }
 
-    $scope.instagram = function() {
-      var url = 'https://api.instagram.com/oauth/authorize/?client_id=de7a9c72311142369179ad6b5b880031&scope=public_content&redirect_uri=http://yutrippin.herokuapp.com/&response_type=token';
-      window.location.replace(url);
-    }
-
     $scope.fetchPhotos = function(){
-      $http.jsonp('https://api.instagram.com/v1/tags/chi/media/recent?access_token='+ $scope.auth + '&callback=JSON_CALLBACK').then(function(response){
-        $scope.instaPics = response.data.data.filter(function(type){
-          return type.type == "image";
-        })
-        .map(function(image){
-         return image.images.standard_resolution.url;
+      $http.jsonp('https://api.flickr.com/services/rest/?method=flickr.photos.search&text=-virtual -mesh vscocam '+ $scope.query +' -SL&format=json&jsoncallback=JSON_CALLBACK&api_key=d89fbfe1199130026c28dee84a78c7ef&sort=interestingness-desc').then(function(response){
+        console.log(response);
+        $scope.flickrPics = response.data.photos.photo;
+        $scope.flickrPics = $scope.flickrPics.map(function(image){
+         return 'https://farm'+ image.farm +'.staticflickr.com/'+ image.server + '/' + image.id + '_' + image.secret +'.jpg';
        })
-
         $scope.slicedPics = [];
 
-        while($scope.instaPics.length) {
-          $scope.slicedPics.push($scope.instaPics.splice(0,6));
+        while($scope.flickrPics.length) {
+          $scope.slicedPics.push($scope.flickrPics.splice(0,6));
         }
       })
     }
 
     $scope.soundcloud = function (){
       $http.get('http://api.soundcloud.com/tracks?client_id=2596b97e0994cfe28ac4f0a9a1f5761a&tag_list=' + $scope.query +'&q=' + $scope.query).then(function(response){
-            $scope.sounds = response.data.map(function(song){
-            return {
+        $scope.sounds = response.data.map(function(song){
+          return {
             id: song.id,
             title: song.title,
             artist: song.user.username,
             url: song.stream_url + '?client_id=2596b97e0994cfe28ac4f0a9a1f5761a'
-            }
+          }
         });
       })
     }
